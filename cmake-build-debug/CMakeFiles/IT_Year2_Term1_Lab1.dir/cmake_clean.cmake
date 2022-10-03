5716887a12bc3115702c72b149508efb0bffc9f8
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2022-10-03 18:32:27 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/IT_Year2_Term1_Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IT_Year2_Term1_Lab1.dir/main.cpp.o"
+  "IT_Year2_Term1_Lab1"
+  "IT_Year2_Term1_Lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IT_Year2_Term1_Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
